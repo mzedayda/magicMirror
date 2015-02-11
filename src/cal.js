c7672f565cbf2f2660fb829e856b9492433ad901
mzedayda <mzedayda@gmail.com>
2015-02-12 00:01:13 +0200

    Display google calendar today's events (still messy)

--- src/cal.js
+++ src/cal.js
@@ -25,17 +25,55 @@ function handleAuthClick(event) {
   gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
 }
 
+function today() {
+  date = new Date();
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+}
+
+function tomorrow() {
+  // Not good, should be replaced
+  date = new Date();
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+}
+
+function insertRow(event) {
+  var docEvents = document.getElementById('events');
+  var tableRow = document.createElement('tr');
+  var eventTime = document.createElement('td');
+  startHour = moment(new Date(event.start.dateTime || event.start.date)).format("HH:mm");
+  eventTime.appendChild(document.createTextNode(startHour));
+  eventTime.setAttribute("class", "eventTime small");
+  var eventTitle = document.createElement('td');
+  eventTitle.appendChild(document.createTextNode(event.summary));
+  eventTitle.setAttribute("class", "eventTitle xxsmall");
+  tableRow.appendChild(eventTitle);
+  tableRow.appendChild(eventTime);
+  docEvents.appendChild(tableRow);
+}
+
 function makeApiCall() {
   gapi.client.load('calendar', 'v3').then(function() {
-    var request = gapi.client.calendar.calendarList.list();
-    request.then(function(resp) {
-      var section = document.createElement('section');
-      resp.result.items.forEach(function(calendar) {
-        section.appendChild(document.createTextNode(calendar.id + " \n"));
-      });
-      document.getElementById('events').appendChild(section);
-    }, function(reason) {
-      console.log('Error: ' + reason.result.error.message);
-    });
+    function getTodayEvents() {
+      console.log("Called");
+      var request = gapi.client.calendar.calendarList.list();
+      request.then(function(resp) {
+        resp.result.items.forEach(function(calendar) {
+          min = today().toISOString();
+          max = tomorrow().toISOString();
+          var eventsRequest = gapi.client.calendar.events.list({calendarId: calendar.id, singleEvents: true, timeMin: min, timeMax: max});
+          eventsRequest.then(function (resp2) {
+            resp2.result.items.forEach(function(event) {
+              insertRow(event);
+            });
+          }, function(reason) {
+            console.log('Error: ' + reason.result.error.message);
+          });
+        });
+      }, function(reason) {
+        console.log('Error: ' + reason.result.error.message);
+      });  
+      setTimeout(getTodayEvents, 6000000);   
+    }
+    getTodayEvents();
   });
 }
